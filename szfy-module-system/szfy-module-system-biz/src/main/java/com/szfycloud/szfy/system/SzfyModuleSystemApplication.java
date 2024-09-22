e28d661c9d55c8c49845f27b34de03c126fe64da
Ananas <zhangchenglong@szfycloud.com>
2024-09-22 16:17:54 +0800

    项目初始化

---
+++ szfy-module-system/szfy-module-system-biz/src/main/java/com/szfycloud/szfy/system/SzfyModuleSystemApplication.java
@@ -0,0 +1,19 @@
+package com.szfycloud.szfy.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 系统服务启动类
+ *
+ * @author Ananas
+ * @date 2024/09/22
+ */
+@SpringBootApplication
+public class SzfyModuleSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzfyModuleSystemApplication.class, args);
+    }
+
+}
